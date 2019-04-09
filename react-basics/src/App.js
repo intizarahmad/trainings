81aea68220a527e3b87e0ed5137660062682223d
Intizar Ahmad <intizar.ahmad@telus.com>
2019-04-09 15:34:42 +0530

    static component inserted

---
+++ react-basics/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Greetings from './Greetings';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Greetings />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-basics/src/Greetings.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const greetings = ()=><h1>Hello Team</h1>
+export default greetings;
