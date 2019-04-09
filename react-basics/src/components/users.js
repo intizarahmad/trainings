30b4ff97c68720f73ef8052cae5e492338d78dcf
Intizar Ahmad <intizar.ahmad@telus.com>
2019-04-09 17:05:50 +0530

    data filled from state

--- react-basics/src/components/users.js
+++ react-basics/src/components/users.js
@@ -3,27 +3,51 @@ import './users.css';
 class Users extends Component {
     constructor(props) {
         super(props);
+        this.state ={
+            usersDetails : [
+                {
+                    id: '1',
+                    name :'Intizar Ahmad',
+                    designation: 'ML',
+                    skills: 'React Js, NodeJs angular JS'
+                },
+                {
+                    id: '1',
+                    name :'Akash Agarwal',
+                    designation: 'SSE',
+                    skills: 'React Js, NodeJs angular JS'
+                },
+                {
+                    id: '1',
+                    name :'Ravi Yadav',
+                    designation: 'ML',
+                    skills: 'React Js, NodeJs angular JS'
+                }
+            ]
+        } 
     }
+
     render() {
+        const {usersDetails} = this.state;
         return (
             <div>
                 <div className="card">
-                    <h1>John Doe</h1>
-                    <p className="title">CEO & Founder, Example</p>
-                    <p>Harvard University</p>
-                    <p><button>Contact</button></p>
+                    <h1>{usersDetails['0'].name}</h1>
+                    <p className="title">{usersDetails['0'].designation}</p>
+                    <p>{usersDetails['0'].skills}</p>
+                    <p><button>Delete</button></p>
                 </div>
                 <div className="card">
-                    <h1>John Doe</h1>
-                    <p className="title">CEO & Founder, Example</p>
-                    <p>Harvard University</p>
-                    <p><button>Contact</button></p>
+                    <h1>{usersDetails['1'].name}</h1>
+                    <p className="title">{usersDetails['1'].designation}</p>
+                    <p>{usersDetails['0'].skills}</p>
+                    <p><button>Delete</button></p>
                 </div>
                 <div className="card">
-                    <h1>John Doe</h1>
-                    <p className="title">CEO & Founder, Example</p>
-                    <p>Harvard University</p>
-                    <p><button>Contact</button></p>
+                    <h1>{usersDetails['2'].name}</h1>
+                    <p className="title">{usersDetails['2'].designation}</p>
+                    <p>{usersDetails['0'].skills}</p>
+                    <p><button>Delete</button></p>
                 </div>
             </div>
         );
