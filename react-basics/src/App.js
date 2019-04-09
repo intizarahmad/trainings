741852d0aab6725a2d9ac855c5088d26523ca149
Intizar Ahmad <intizar.ahmad@telus.com>
2019-04-09 19:42:30 +0530

    Route component added

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'; 
 import './App.css';
 import Greetings from './Greetings';
 import Users from './components/users/users';
 import Header from './components/header/header';
-
+import Home from './components/home/home';
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
        {/* <Greetings /> */}
        <Header />
-       <Users />
+       {/* <Users /> */}
+       
+        <Route exact path="/" component={Home} />
+        <Route path="/home/" component={Home} />
+        <Route path="/users/" component={Users} />
       </div>
+      </BrowserRouter>
     );
   }
 }
--- react-basics/src/components/header/header.js
+++ react-basics/src/components/header/header.js
@@ -3,7 +3,7 @@ import './header.css';
 const header = () => {
     return (
         <div class="topnav">
-            <a class="active" href="#home">Home</a>
+            <a class="active" href="home">Home</a>
             <a href="users">Users</a>
       </div>
     );
